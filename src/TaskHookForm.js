65e450e2f62e38262773a9e85458b9abd3c4547b
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-27 05:01:32 +0300

    react-hook-form setup

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function TaskHookForm() {
+  const { register, handleSubmit } = useForm();
   return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
-  )
+    <div>
+      Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm
+      dosyasındaki HTML yapısını vs app.css içerisindeki classları
+      kullanabilirsiniz.
+    </div>
+  );
 }
