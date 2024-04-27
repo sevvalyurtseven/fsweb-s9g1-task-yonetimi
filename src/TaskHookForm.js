2635bee3cf0d230e1be8d8bae022b038383c9ffa
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-27 05:05:36 +0300

    add default values

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -2,7 +2,17 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function TaskHookForm() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      title: "",
+      description: "",
+      people: [],
+    },
+  });
   return (
     <div>
       Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm
