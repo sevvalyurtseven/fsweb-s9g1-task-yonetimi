b7dd42df9590dcd7b53523294160196d27c02a09
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-27 05:17:32 +0300

    handleSubmit

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -27,10 +27,65 @@ export default function TaskHookForm({ kisiler, submitFn }) {
     event.target.reset();
   };
   return (
-    <div>
-      Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm
-      dosyasındaki HTML yapısını vs app.css içerisindeki classları
-      kullanabilirsiniz.
-    </div>
+    <form className="taskForm" onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-line">
+        <label className="input-label" htmlFor="title">
+          Başlık
+        </label>
+        <input
+          className="input-text"
+          id="title"
+          name="title"
+          type="text"
+          onChange={handleOthersChange}
+          value={formData.title}
+        />
+        <p className="input-error">{formErrors.title}</p>
+      </div>
+
+      <div className="form-line">
+        <label className="input-label" htmlFor="description">
+          Açıklama
+        </label>
+        <textarea
+          className="input-textarea"
+          rows="3"
+          id="description"
+          name="description"
+          onChange={handleOthersChange}
+          value={formData.description}
+        ></textarea>
+        <p className="input-error">{formErrors.description}</p>
+      </div>
+
+      <div className="form-line">
+        <label className="input-label">İnsanlar</label>
+        <div>
+          {kisiler.map((p) => (
+            <label className="input-checkbox" key={p}>
+              <input
+                type="checkbox"
+                name="people"
+                value={p}
+                onChange={handleCheckboxChange}
+                checked={formData.people.includes(p)}
+              />
+              {p}
+            </label>
+          ))}
+        </div>
+        <p className="input-error">{formErrors.people}</p>
+      </div>
+
+      <div className="form-line">
+        <button
+          className="submit-button"
+          type="submit"
+          disabled={buttonDisabled}
+        >
+          Kaydet
+        </button>
+      </div>
+    </form>
   );
 }
